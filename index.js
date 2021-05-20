d8ea9d7a2852d97a4140e1ec9873a9fb2106945b
Linea <line301u@stud.kea.dk>
2021-05-20 13:44:10 +0200

    styling home page

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 import "./sass/style.scss";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  getData();
+}
+
+async function getData() {
+  let url = "https://hold-kaeft-vi-har-det-godt.herokuapp.com/beertypes";
+  let jsonData = await fetch(url);
+  jsonData = await jsonData.json();
+  console.log({ jsonData });
+
+  let container = document.querySelector("#beerlist_container");
+  let temp = document.querySelector("template");
+
+  jsonData.forEach((beer) => {
+    const clone = temp.cloneNode(true).content;
+
+    const beerName = beer.name;
+    clone.querySelector(".beer_image").src = `beer_images/${beerName}.png`;
+    clone.querySelector(".beer_name").textContent = beer.name;
+    clone.querySelector(".price").textContent = "40,-";
+    clone.querySelector(".alc").textContent = beer.alc + "% alc.";
+
+    // clone.querySelector("#beerlist_container").addEventListener("click", () => showMore(beer));
+
+    container.appendChild(clone); //klones til DOM
+  });
+}
