6e3afcae20e874c41f0af2e975e5b78423df85ea
Linea <line301u@stud.kea.dk>
2021-05-21 16:58:11 +0200

    styling on singleview/desciption buttons works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "./sass/style.scss";
 
 window.addEventListener("DOMContentLoaded", init);
+let filter = "flavor";
 
 function init() {
   getData();
@@ -26,7 +27,7 @@ async function getData() {
 
     clone.querySelector(".template-article").addEventListener("click", () => showDetails(beer, beerName));
 
-    container.appendChild(clone); //klones til DOM
+    container.appendChild(clone);
   });
 }
 
@@ -36,12 +37,35 @@ function showDetails(beer, beerName) {
 
   details.querySelector(".sv_beer_image").src = `beer_images/${beerName}.png`;
   details.querySelector(".sv_beer_name").textContent = beer.name;
+  details.querySelector(".sv_alc").textContent = beer.alc + "% alc.";
   details.querySelector(".description").textContent = beer.description.overallImpression;
   details.querySelector(".sv_price").textContent = "40,-";
+
   details.querySelector(".aroma_desc").textContent = beer.description.aroma;
-  details.querySelector(".appearence_desc").textContent = beer.description.appearence;
+  details.querySelector(".appearence_desc").textContent = beer.description.appearance;
   details.querySelector(".flavor_desc").textContent = beer.description.flavor;
   details.querySelector(".mouthfeel_desc").textContent = beer.description.mouthfeel;
 
   document.querySelector(".close_singleview").addEventListener("click", () => (details.style.display = "none"));
+  addEventListenerToButtons();
+}
+
+function addEventListenerToButtons() {
+  document.querySelectorAll(".description_tabs button").forEach((btn) => {
+    btn.addEventListener("click", filterDesc);
+  });
+}
+
+function filterDesc() {
+  filter = this.dataset.field;
+  document.querySelectorAll(".description_tabs button").forEach((btn) => {
+    btn.classList.remove("chosen_desc");
+  });
+
+  document.querySelectorAll(".desc_wrapper p").forEach((p) => {
+    p.classList.add("hidden");
+  });
+
+  this.classList.add("chosen_desc");
+  document.querySelector(`.${filter}_desc`).classList.remove("hidden");
 }
