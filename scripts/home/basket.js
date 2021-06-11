2c7480bc00cabdeb6cd10d17dbdc8a4e2a1fae43
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-06-11 13:10:52 +0200

    IIf< 1 minus button click -> remove from basket feature

--- scripts/home/basket.js
+++ scripts/home/basket.js
@@ -1,152 +1,155 @@
 import { basket } from "./order";
 export function displayTotal() {
-  let priceCount = 0;
+    let priceCount = 0;
 
-  document.querySelectorAll(".subtotal").forEach((element) => {
-    let itemTotal = parseInt(element.textContent);
-    priceCount += itemTotal;
-  });
+    document.querySelectorAll(".subtotal").forEach((element) => {
+        let itemTotal = parseInt(element.textContent);
+        priceCount += itemTotal;
+    });
 
-  document.querySelector(".total_price").textContent = `${priceCount},-`;
-  getBasketNumber(priceCount);
+    document.querySelector(".total_price").textContent = `${priceCount},-`;
+    getBasketNumber(priceCount);
 }
 
 function getBasketNumber(priceCount) {
-  let basketNumber = priceCount / 40;
+    let basketNumber = priceCount / 40;
 
-  if (basketNumber < 1) {
-    document.querySelector(".basket_number").classList.add("hidden");
-  } else {
-    document.querySelector(".basket_number").classList.remove("hidden");
-    document.querySelector(".basket_number").textContent = basketNumber;
-  }
+    if (basketNumber < 1) {
+        document.querySelector(".basket_number").classList.add("hidden");
+    } else {
+        document.querySelector(".basket_number").classList.remove("hidden");
+        document.querySelector(".basket_number").textContent = basketNumber;
+    }
 }
 
 export function calculateBasketAmount(basket_item_name) {
-  document.querySelector(`.${basket_item_name} .plus_basket`).addEventListener("click", () => plusBasket());
-  document.querySelector(`.${basket_item_name} .minus_basket`).addEventListener("click", () => minusBasket());
+    document.querySelector(`.${basket_item_name} .plus_basket`).addEventListener("click", () => plusBasket());
+    document.querySelector(`.${basket_item_name} .minus_basket`).addEventListener("click", minusBasket);
 
-  let basketCounter = document.querySelector(`.${basket_item_name} .basket_amount`);
-  let basketCount = basketCounter.value;
+    let basketCounter = document.querySelector(`.${basket_item_name} .basket_amount`);
+    let basketCount = basketCounter.value;
 
-  function plusBasket() {
-    basketCount++;
-    basketCounter.value = basketCount;
+    function plusBasket() {
+        basketCount++;
+        basketCounter.value = basketCount;
 
-    let newBasketPrice = 40 * basketCount;
-    let newnewPrice = newBasketPrice.toString();
+        let newBasketPrice = 40 * basketCount;
+        let newnewPrice = newBasketPrice.toString();
 
-    document.querySelector(`.${basket_item_name} .subtotal`).textContent = `${newnewPrice},-`;
-    document.querySelector(`.${basket_item_name} .minus_basket`).style.backgroundColor = "white";
+        document.querySelector(`.${basket_item_name} .subtotal`).textContent = `${newnewPrice},-`;
+        document.querySelector(`.${basket_item_name} .minus_basket`).style.backgroundColor = "white";
 
-    displayTotal();
-  }
-  function minusBasket() {
-    if (basketCount > 1) {
-      basketCount--;
-      basketCounter.value = basketCount;
-
-      let newBasketPrice = 40 * basketCount;
-      let newnewPrice = newBasketPrice.toString();
-
-      document.querySelector(`.${basket_item_name} .subtotal`).textContent = `${newnewPrice},-`;
-
-      displayTotal();
-
-      if (basketCount < 2) {
-        document.querySelector(`.${basket_item_name} .minus_basket`).style.backgroundColor = "#f4f4f4";
         displayTotal();
-      }
     }
-  }
+    function minusBasket() {
+        if (basketCount > 1) {
+            basketCount--;
+            basketCounter.value = basketCount;
+
+            let newBasketPrice = 40 * basketCount;
+            let newnewPrice = newBasketPrice.toString();
+
+            document.querySelector(`.${basket_item_name} .subtotal`).textContent = `${newnewPrice},-`;
+
+            displayTotal();
+
+            if (basketCount < 2) {
+                document.querySelector(`.${basket_item_name} .minus_basket`).style.backgroundColor = "#f4f4f4";
+                displayTotal();
+            }
+        } else {
+            const basketItem = this.closest("li[data-field]");
+            removeBasketItem(basketItem, basket_item_name);
+        }
+    }
 }
 
 export function removeBasketItem(basketItem, basket_item_name) {
-  document.querySelector(`.remove_${basket_item_name}`).removeEventListener("click", removeBasketItem);
+    document.querySelector(`.remove_${basket_item_name}`).removeEventListener("click", removeBasketItem);
 
-  // animate item out
-  document.querySelector(`.${basket_item_name}`).classList.add("backOutRight");
+    // animate item out
+    document.querySelector(`.${basket_item_name}`).classList.add("backOutRight");
 
-  setTimeout(() => {
-    basketItem.remove();
-  }, 1000);
+    setTimeout(() => {
+        basketItem.remove();
+    }, 1000);
 
-  basket[basketItem.dataset.field] = false;
+    basket[basketItem.dataset.field] = false;
 
-  checkIfBasketIsEmpty();
-  displayTotal();
+    checkIfBasketIsEmpty();
+    displayTotal();
 }
 
 export function createAddedElement(beer, basket_item_name) {
-  const price = 40 * document.querySelector(".amount").value + ",-";
+    const price = 40 * document.querySelector(".amount").value + ",-";
 
-  const li = document.createElement("li");
-  li.classList.add(basket_item_name);
-  li.dataset.field = beer.name;
+    const li = document.createElement("li");
+    li.classList.add(basket_item_name);
+    li.dataset.field = beer.name;
 
-  const content = document.createElement("div");
-  content.classList.add("content");
+    const content = document.createElement("div");
+    content.classList.add("content");
 
-  const img = document.createElement("img");
-  img.src = `beer_images_with_circle/${beer.name}.png`;
-  content.append(img);
+    const img = document.createElement("img");
+    img.src = `beer_images_with_circle/${beer.name}.png`;
+    content.append(img);
 
-  const div_text = document.createElement("div");
-  const div_amount = document.createElement("div");
-  content.append(div_text);
-  div_text.append(div_amount);
+    const div_text = document.createElement("div");
+    const div_amount = document.createElement("div");
+    content.append(div_text);
+    div_text.append(div_amount);
 
-  let text = ["40,-", beer.name];
-  text.forEach(function (el) {
-    const p = document.createElement("p");
-    p.textContent = el;
-    p.classList.add("basket_text");
-    div_text.append(p);
-  });
+    let text = ["40,-", beer.name];
+    text.forEach(function (el) {
+        const p = document.createElement("p");
+        p.textContent = el;
+        p.classList.add("basket_text");
+        div_text.append(p);
+    });
 
-  const totalAmount = document.createElement("p");
-  totalAmount.textContent = price;
-  totalAmount.classList.add("subtotal");
-  content.append(totalAmount);
+    const totalAmount = document.createElement("p");
+    totalAmount.textContent = price;
+    totalAmount.classList.add("subtotal");
+    content.append(totalAmount);
 
-  const minus_button = document.createElement("button");
-  minus_button.textContent = "-";
-  minus_button.classList.add("plus_minus", "minus_basket");
-  div_amount.append(minus_button);
+    const minus_button = document.createElement("button");
+    minus_button.textContent = "-";
+    minus_button.classList.add("plus_minus", "minus_basket");
+    div_amount.append(minus_button);
 
-  const input = document.createElement("input");
-  const amount = document.querySelector(".amount").value;
-  input.classList.add("basket_amount");
-  div_amount.append(input);
+    const input = document.createElement("input");
+    const amount = document.querySelector(".amount").value;
+    input.classList.add("basket_amount");
+    div_amount.append(input);
 
-  input.value = amount;
-  input.disabled = true;
+    input.value = amount;
+    input.disabled = true;
 
-  const plus_button = document.createElement("button");
-  plus_button.textContent = "+";
-  plus_button.classList.add("plus_minus", "plus_basket");
-  div_amount.append(plus_button);
+    const plus_button = document.createElement("button");
+    plus_button.textContent = "+";
+    plus_button.classList.add("plus_minus", "plus_basket");
+    div_amount.append(plus_button);
 
-  const button = document.createElement("button");
-  button.classList.add("remove_added_beer", `remove_${basket_item_name}`);
-  content.append(button);
+    const button = document.createElement("button");
+    button.classList.add("remove_added_beer", `remove_${basket_item_name}`);
+    content.append(button);
 
-  li.append(content);
+    li.append(content);
 
-  return li;
+    return li;
 }
 
 function checkIfBasketIsEmpty() {
-  const basketListItems = document.querySelectorAll(".added_beers ul li");
-  console.log(basketListItems);
+    const basketListItems = document.querySelectorAll(".added_beers ul li");
+    console.log(basketListItems);
 
-  for (let i = 0; i <= basketListItems.length; i++) {
-    if (basketListItems.length === 1) {
-      document.querySelector(".basket_pay").style.opacity = 0.2;
+    for (let i = 0; i <= basketListItems.length; i++) {
+        if (basketListItems.length === 1) {
+            document.querySelector(".basket_pay").style.opacity = 0.2;
 
-      setTimeout(() => {
-        document.querySelector(".empty_basket").style.display = "block";
-      }, 1000);
+            setTimeout(() => {
+                document.querySelector(".empty_basket").style.display = "block";
+            }, 1000);
+        }
     }
-  }
 }
